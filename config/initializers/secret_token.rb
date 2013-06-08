4046e84f07754ff2dff3d37029c0cb622dda8d2e
Thomas Atwood <tom@sandhilldigital.com>
2013-06-08 14:30:49 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nutra::Application.config.secret_token = 'b1aa7c167ce0206f64ca751a217ef9ae781072ac507eb7fdb096a0af4be980090bc99e593cc3937b06c7c5d827044f95fb5f1e82d3456385cd5dcee2b6c6dcb7'
